b98f519b4d8ee42c8751f9424862e92f9b0c6977
Matin Kaboli <matinkaboli@aol.com>
2024-07-29 13:06:30 +0330

    chore: add exports_abi script

---
+++ export_abis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Deleting the abi/ directory"
+rm -rf abi
+
+echo "Creating the abi/ directory"
+mkdir abi
+
+echo "Exporting ABIs to abi/ directory"
+forge inspect Burner abi > abi/BURNER_ABI.json
+forge inspect Game abi > abi/GAME.json
+forge inspect GameFactory abi > abi/GAME_FACTORY_ABI.json
+forge inspect MONT abi > abi/ERC20_ABI.json
+forge inspect MontRewardManager abi > abi/MONT_REWARD_MANAGER_ABI.json
+forge inspect Revealer abi > abi/REVEALER_FACTORY_ABI.json
+forge inspect TaxBasedLocker abi > abi/TAX_BASED_LOCKER_ABI.json
+forge inspect Vault abi > abi/VAULT_ABI.json
+
